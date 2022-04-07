f4d04581857bc15180dad8c68a267939623d04ae
Nico <johnnicomolon@yahoo.com>
2022-04-07 17:32:54 +0800

    Component Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
-      <Login />
+      {/* <Login /> */}
+      <Home />
     </div>
   );
 }
